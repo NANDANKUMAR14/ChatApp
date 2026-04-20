995eba973b4a8e74c507d1d7c77c89b27201e84e
Nandankumar S <nandan1452007@gmail.com>
2026-04-20 15:20:19 +0530

    added socket.io for real time messaging feature

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,24 +1,29 @@
 import { Request, Response } from "express";
 import { Message } from "../models/message";
 import { Chat } from "../models/chat";
+import { getIO } from "../sockets/socket";
 
 export const sendMessage = async (req: Request, res: Response) => {
   const { content, chatId, senderId } = req.body;
 
   try {
+    const io = getIO();
+
     const message = await Message.create({
       sender: senderId,
       content,
       chat: chatId,
     });
 
-    const populatedMessage = await message.populate("sender", "name email");
+    const fullMessage = await message.populate("sender", "name email");
 
     await Chat.findByIdAndUpdate(chatId, {
       latestMessage: message._id,
     });
+      // 🔥 Emit to socket
+    io.to(chatId).emit("message received", fullMessage);
 
-    res.json(populatedMessage);
+    res.json(fullMessage);
   } catch (error) {
     res.status(500).json({ message: "Message send failed" });
   }
--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,64 @@ import { connectDB } from "./config/db";
 import authRoutes from "./routes/AuthRoute";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
+import http from "http";
+import { initSocket } from "./sockets/socket";
 
-const app = express();
+
 dotenv.config({ path: "./src/.env" });
+const app = express();
 connectDB();
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+//routes
+
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages",messageRoutes);
+
+// 🔥 Create HTTP server
+const server = http.createServer(app);
+
+// 🔥 Attach Socket.IO
+const io = initSocket(server);
+
+// 🔥 Socket logic
+io.on("connection", (socket) => {
+  console.log("🔥 User connected:", socket.id);
+
+  // Join room (user specific)
+  socket.on("setup", (userId) => {
+    socket.join(userId);
+    console.log("User joined:", userId);
+  });
+
+  // Join chat room
+  socket.on("join chat", (chatId) => {
+    socket.join(chatId);
+    console.log("Joined chat:", chatId);
+  });
+
+  // Send message
+  socket.on("new message", (message) => {
+    const chat = message.chat;
+
+    if (!chat.users) return;
+
+    chat.users.forEach((user: any) => {
+      if (user._id === message.sender._id) return;
+
+      socket.to(user._id).emit("message received", message);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ User disconnected");
+  });
+});
+
 
 
 // Test route
@@ -25,12 +70,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Test route working" });
-});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
